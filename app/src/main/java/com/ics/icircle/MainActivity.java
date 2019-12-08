a7de8035a7e5f80612142177879c1b669dcb60e2
ARNAB <arnab@93.1@gmail.com>
2019-12-08 20:54:37 +0530

    Commit 1

---
+++ app/src/main/java/com/ics/icircle/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ics.icircle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //placing toolbar in place of actionbar
+        setSupportActionBar(toolbar);
+    }
+    
+}
